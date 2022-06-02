37bfbebcdd46c00078519599d0d8e6b6a47f4b73
chovy <anthony@chovy.com>
2022-06-02 00:46:39 -0700

    add blog stuff

---
+++ src/components/News.svelte
@@ -0,0 +1,96 @@
+<script>
+	import BlogListItem from './blog/BlogListItem.svelte';
+	import posts from '../routes/blog/_posts';
+
+	console.log(posts.length);
+	// export let title;
+</script>
+
+<section id="news" class="news-block">
+	<a href="/blog" class="any-news">Latest news</a>
+
+	<p class="title-p">
+		HSKO promises a very ambitious roadmap, which will be focused on rewarding those who share the most content in the community.
+	</p>
+
+	<div class="news">
+		<BlogListItem post={posts[0]} />
+		<BlogListItem post={posts[7]} />
+	</div>
+</section>
+
+<style>
+	.news {
+		display: flex;
+		justify-content: space-between;
+		text-align: left;
+		color: #fff;
+	}
+
+	.any-news {
+		font-family: Furore;
+		font-size: 4.2rem;
+		font-weight: 400;
+		transition: 0.5s;
+	}
+
+	.any-news:hover {
+		transition: 0.5s;
+		transform: scale(1.01);
+	}
+
+	.news-block {
+		padding: 10rem 4rem 7.7rem 4rem;
+		max-width: 124.5rem;
+		width: 100%;
+		margin: 0 auto;
+	}
+
+	.news-block a {
+		display: flex;
+		color: #fff;
+	}
+
+	.title-p {
+		width: 66rem;
+		margin: 1rem 0 3rem 0;
+		text-align: left;
+	}
+
+	@media (max-width: 820px) {
+		.news-block{
+			padding: 5rem 3rem 1rem 3rem;
+			width: 100%;
+		}
+
+		.news{
+			display: block;
+		}
+
+		.title-p{
+			width: 50rem;
+			margin: 1.5rem auto 3rem auto;
+			text-align: center;
+		}
+
+		.any-news{
+			font-size: 3.6rem;
+			justify-content: center;	
+		}
+	}
+
+	@media (max-width: 580px) {
+
+		.news-block{
+			padding: 6rem 3rem 1rem 3rem;
+			width: 100%;
+		}
+
+		.title-p{
+			/* max-width: 32rem; */
+			width: 90%;
+			margin: 2rem auto 4rem auto;
+			text-align: center;
+		}
+	}
+</style>
---
+++ src/components/blog/BlogListItem.svelte
@@ -0,0 +1,115 @@
+<script>
+	export let post;
+</script>
+
+<article>
+	<a href={`/blog/${post.slug}`}>
+		<div class="post-content">
+			<div
+				class="post-image"
+				style={`background-image: url(${
+					post.image || `https://picsum.photos/980/492?random=${Math.random()}`
+				})`}
+			/>
+			<div class="right-container">
+				<span class="post-category">{post.category || 'news'}</span>
+				<h2 class="post-title">{post.title}</h2>
+				<span class="post-text">{@html post.html}</span>
+				<span class="post-date">
+					{post.timestamp
+						? new Date(post.timestamp).toLocaleDateString()
+						: new Date().toLocaleDateString()}
+				</span>
+			</div>
+		</div>
+	</a>
+</article>
+
+<style>
+	article {
+		width: 49%;
+		padding: 1em 0;
+		margin: 0.5em 0;
+		transition: transform 0.2s ease;
+	}
+	article:hover {
+		transform: scale(1.015);
+	}
+	.post-title {
+		font-size: 2.5rem;
+		color: #fff;
+	}
+	.post-text {
+		color: #8794a7;
+		text-align: left;
+		height: 6rem;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
+	}
+	.post-content {
+		display: flex;
+		flex-direction: column;
+	}
+	.post-image {
+		flex-shrink: 0;
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		width: 100%;
+		height: 30rem;
+	}
+	.right-container {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+	}
+	.post-category {
+		text-transform: uppercase;
+		font-size: 1.2rem;
+		color: #fff;
+		background: #000;
+		padding: 0.3rem 0.5rem;
+		margin: 0.7rem 0;
+	}
+	.post-date {
+		color: rgba(255, 255, 255, 0.637);
+		font-size: 1.3rem;
+		margin-top: 0.5rem;
+	}
+	h2 {
+		letter-spacing: inherit;
+	}
+
+	@media (max-width: 820px) {
+
+		article{
+			padding: 0 2rem;
+			width: 100%;
+			margin: 1rem 0 4rem 0;
+		}
+
+		.post-image{
+			height: 30rem;
+		}
+
+
+	}
+
+	@media (max-width: 412px) {
+
+		article{
+			padding: 0;
+			margin-bottom: 5rem;
+		}
+
+		.post-image{
+			height: 22rem;
+		}
+
+	}
+
+
+</style>
