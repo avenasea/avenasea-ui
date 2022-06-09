ee81d8e5399342725222623975e5ef8f8e32c3f9
Louis Huddleston <louishuddleston@gmail.com>
2022-06-09 16:22:16 +0100

    create schema testing page

---
+++ src/routes/schema@blank.svelte
@@ -0,0 +1,72 @@
+<script>
+	import Ajv from 'ajv';
+
+	const ajv = new Ajv({ allErrors: true, strict: false });
+
+	let schema = '';
+	let schemaObject = {};
+
+	let finalObject = {};
+	let finalJSON = '';
+
+	let errors = [];
+
+	const validateAll = () => {
+		try {
+			errors = [];
+			schemaObject = schema ? JSON.parse(schema) : {};
+			finalJSON = finalObject ? JSON.stringify(finalObject) : '';
+			const validate = ajv.compile(schemaObject);
+			const valid = validate(finalObject);
+			if (!valid) {
+				console.log(validate.errors);
+				errors = validate.errors;
+			}
+		} catch (error) {
+			errors.push(error.message);
+			console.error(error);
+		}
+	};
+</script>
+
+<div class="container">
+	<div class="text-container">
+		<label for="schema">Paste schema here:</label>
+		<textarea bind:value={schema} name="schema" id="schema" cols="30" rows="10" />
+		<label for="finalJSON">Object output:</label>
+		<textarea bind:value={finalJSON} name="finalJSON" id="finalJSON" cols="30" rows="10" />
+	</div>
+	<button on:click={validateAll}>Validate</button>
+	{#each errors as error}
+		<p class="error">{JSON.stringify(error)}</p>
+	{/each}
+	<form on:submit|preventDefault={() => {}}>
+		{#if schemaObject.properties}
+			{#each Object.entries(schemaObject.properties) as [key, val]}
+				<fieldset>
+					<label for="">{key}</label>
+					{#if errors.filter((e) => e.instancePath?.includes(key)).length > 0}
+						<div class="error">{errors.filter((e) => e.instancePath.includes(key))[0].message}</div>
+					{/if}
+					{#each Object.entries(val) as [key, val]}
+						<p>{key}: {val}</p>
+					{/each}
+					{#if val.type == 'number'}
+						<input type="number" bind:value={finalObject[key]} />
+					{:else}
+						<input type="text" bind:value={finalObject[key]} />
+					{/if}
+				</fieldset>
+			{/each}
+		{/if}
+	</form>
+</div>
+
+<style>
+	.container {
+		margin: 4em;
+	}
+	.text-container {
+		display: flex;
+	}
+</style>
