4a975ba0e3b4ab08c2e872f1d8e5cc8323c88644
chovy <anthony@chovy.com>
2022-06-10 07:25:47 -0700

    phone

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,17 +3,19 @@
 	const domain = import.meta.env.VITE_META_DOMAIN;
 	const twitter = import.meta.env.VITE_META_TWITTER;
 </script>
+
 <footer>
 	<div class="footer">
 		<div class="logo">
-
 			<p>
 				Say <a href="mailto:hello@{domain}?subject={name}">hello@{domain}</a> -
 				<a href="https://twitter.com/{twitter}">@{twitter}</a>
+				Phone: <a href="tel:18886902204">1-888-690-2204</a>
 			</p>
 			<p>
 				<small
-					>&copy; 2021-2022 {name} - <a href="/privacy">Privacy</a> - <a href="/terms">Terms</a></small
+					>&copy; 2021-2022 {name} - <a href="/privacy">Privacy</a> -
+					<a href="/terms">Terms</a></small
 				>
 			</p>
 		</div>
@@ -24,12 +26,11 @@
 		</div>
 		<div class="follow-us">
 			<h4>Follow us</h4>
-				<div class="links">
-					<div class="social-link"></div>
-					<div class="social-link"></div>
-				</div>
+			<div class="links">
+				<div class="social-link" />
+				<div class="social-link" />
+			</div>
 		</div>
-		
 	</div>
 </footer>
 
@@ -41,21 +42,21 @@
 		/* margin-top: 4rem; */
 	}
 
-	.footer{
+	.footer {
 		width: 130rem;
 		margin: 0 auto;
 		display: flex;
 		justify-content: space-between;
 	}
 
-	.links{
+	.links {
 		display: flex;
 		justify-content: space-between;
 	}
 
-	.social-link{
+	.social-link {
 		border-radius: 50%;
-		background-color: #0D9F8E;
+		background-color: #0d9f8e;
 		padding: 1.9rem 1.7rem;
 	}
 </style>
