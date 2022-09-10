e0caa3b0167528d697a506f45fb27b09718be630
Louis <60443647+louishuddleston@users.noreply.github.com>
2022-09-10 21:24:23 +0100

    add field filter by statusSummary (#12)

--- src/routes/contracts/[contractID]/+page.svelte
+++ src/routes/contracts/[contractID]/+page.svelte
@@ -10,6 +10,7 @@
 	const { contractID } = data;
 	let contract: Contract;
 	let splitObject = {};
+	let fieldFilter: ContractField['statusSummary'];
 
 	let totalApprovedFields = 0;
 	$: totalApprovedFields = contract?.fields?.filter(
@@ -18,12 +19,24 @@
 
 	onMount(async () => {
 		contract = await new Contracts().getById(contractID);
-		contract.fields.forEach((val) => {
-			if (!splitObject[val.schemaData.module]) splitObject[val.schemaData.module] = {};
+		contract.fields = contract.fields.map((val) => {
 			val.statusSummary = getStatusSummary(val, contract.parties);
+			return val;
+		});
+		filterFields();
+	});
+
+	const filterFields = () => {
+		if (!contract?.fields) return;
+		splitObject = {};
+		let filtered = contract.fields.filter((val) =>
+			fieldFilter ? val.statusSummary == fieldFilter : true
+		);
+		filtered.forEach((val) => {
+			if (!splitObject[val.schemaData.module]) splitObject[val.schemaData.module] = {};
 			splitObject[val.schemaData.module][val.fieldName] = val;
 		});
-	});
+	};
 </script>
 
 {#if contract}
@@ -37,14 +50,35 @@
 				{/each}
 			</ul>
 			<h3>Fields Approved: {totalApprovedFields}/{contract.fields.length}</h3>
+			<br />
 			<h3>Modules:</h3>
-			{#each Object.entries(splitObject) as [moduleName, moduleValue]}
-				<CollapsableSection collapsed={true} heading={moduleName}>
-					{#each Object.entries(moduleValue) as [_, fieldData]}
-						<FieldContainer {fieldData} bind:contract />
-					{/each}
-				</CollapsableSection>
-			{/each}
+			<label class="filter-label" for="filter">Filter:</label>
+			<select
+				class="select"
+				name="filter"
+				id="filter"
+				bind:value={fieldFilter}
+				on:change={filterFields}
+			>
+				<option value={undefined}>None</option>
+				<option value="fullyApproved">Fully Approved</option>
+				<option value="awaitingMe">Awaiting Me</option>
+				<option value="awaitingOther">Awaiting Other</option>
+				<option value="rejectedByMe">Rejected By Me</option>
+				<option value="rejectedByOther">Rejected By Other</option>
+				<option value="unset">Field Value Unset</option>
+			</select>
+			{#if Object.keys(splitObject).length}
+				{#each Object.entries(splitObject) as [moduleName, moduleValue]}
+					<CollapsableSection collapsed={true} heading={moduleName}>
+						{#each Object.entries(moduleValue) as [_, fieldData]}
+							<FieldContainer {fieldData} bind:contract />
+						{/each}
+					</CollapsableSection>
+				{/each}
+			{:else}
+				<p>No fields matching the filter</p>
+			{/if}
 		</form>
 	</div>
 {/if}
@@ -52,5 +86,8 @@
 <style>
 	.container {
 		margin: 4em;
+	}
+	.filter-label {
+		display: inline;
 	}
 </style>
